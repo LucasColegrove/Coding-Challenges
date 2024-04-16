b9679d97d576b8892be884ca6fcb5b4b7aa2ba6d
Lucas Colegrove <78050882+LucasColegrove@users.noreply.github.com>
2024-04-16 02:03:49 -0500

    Added WordReverser
    Much better than other one

---
+++ WordReverser.cs
@@ -0,0 +1,40 @@
+﻿/*
+PROGRAM DESCRIPTION:
+This program reverses each word in a sentence while 
+maintaining the original position of each word.
+
+EXAMPLE OUTPUT:
+there are snakes at the zoo
+ereht era sekans ta eht ooz
+
+AUTHOR: Lucas Colegrove (04/16/2024)    
+*/
+using System;
+
+string input = "there are snakes at the zoo";
+Console.WriteLine(input);
+Console.WriteLine(ReverseSentence(input));
+
+string ReverseWord(string word)
+{
+    string result = "";
+    for(int i = word.Length - 1; i >= 0; i--)
+    {
+        result += word[i];
+    }
+
+    return result;
+}
+
+string ReverseSentence(string input)
+{
+    string result = "";
+    string[] words = input.Split(" "); 
+
+    foreach (string word in words)
+    {
+        result += ReverseWord(word) + " ";
+    }
+
+    return result.Trim();
+}
